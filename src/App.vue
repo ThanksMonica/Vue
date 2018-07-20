06a012905892578b2312a4c386da78c2549dde47
ThanksMonica <543456878@qq.com>
2018-07-20 20:55:46 +0800

    组件的引入与编写

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Hello/>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+ 
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>{{greeting}}</p>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      greeting: "hello"
+    };
+  }
+};
+</script>
+
+
+
